7e1d0e7a50d547a5bda69661e0cbd0a9558a7deb
Nata <JosephAlvarengaBeech@protonmail.com>
2020-04-08 13:07:43 -0700

    username and header

--- chat-application/client.py
+++ chat-application/client.py
@@ -1,3 +1,16 @@
 import errno
 import select
 import socket
+
+HEADER_LENGTH = 10 
+IP = '127.0.0.1'
+PORT = 1234
+
+my_username  = input('Username: ')
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client_socket.connect((IP, PORT))
+# recieved fuctionality wont be blocking
+client_socket.socket.setblocking(False)
+
+username = my_username.encode('utf-8')
+username_header =  f"{len(username):<{HEADER_LENGTH}}".encode('uft-8')
