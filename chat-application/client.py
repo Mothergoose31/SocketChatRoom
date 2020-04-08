816df05f8db0b31fbd18b7d1eee59aaa1c47899b
Nata <JosephAlvarengaBeech@protonmail.com>
2020-04-08 14:34:20 -0700

    chat app without gui

--- chat-application/client.py
+++ chat-application/client.py
@@ -14,3 +14,15 @@ client_socket.socket.setblocking(False)
 
 username = my_username.encode('utf-8')
 username_header =  f"{len(username):<{HEADER_LENGTH}}".encode('uft-8')
+client_socket.send(username_header + username)
+#  main loop for the client, which will be there to accept new messages from the client.
+while True:
+    # an input, but adding  username in there to give it a chat app feel
+    #  before just sending the message, make sure that there is  one
+    message = input(f'{my_username} > ')
+    if message:
+
+        # Encode message to bytes, prepare header and convert to bytes, like for username above, then send
+        message = message.encode('utf-8')
+        message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+        client_socket.send(message_header + message)
